9c74ea25e5ac4587e31c46afef8f2b9953362c99
Diegomoro415 <diego_moro415@hotmail.com>
2023-05-18 20:06:50 +0000

    add account registration - style login, logout and signup

--- my_django_blog/urls.py
+++ my_django_blog/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("my_blog_app.urls"), name='app-urls'),
-    path('summernote/', include('django_summernote.urls')),    
+    path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
